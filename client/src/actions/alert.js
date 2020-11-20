ac548ce1471232c69048751fd12de86b8fe4db63
Edidiong Daniel <eddyenin6@gmail.com>
2020-11-20 16:48:27 +0100

    Remove alert

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -1,16 +1,22 @@
-//import uuid from 'uuid';
+import { v4 as uuid} from 'uuid';
 import { SET_ALERT, REMOVE_ALERT } from './type';
 
-const setAlert = (msg, alertType) => dispatch => {
-   // const id = uuid.v4();
+const setAlert = (msg, alertType, id) => dispatch => {
+    const id = uuid();
     dispatch({
         type: SET_ALERT,
         payload:{
             msg,
-            alertType
+            alertType,
+            id
             
         }
     })
+
+    setTimeout(() => dispatch({
+        type:REMOVE_ALERT,
+        payload:id
+    }), 3000);
 }
 
 export default setAlert;
--- client/src/components/layout/Alert.js
+++ client/src/components/layout/Alert.js
@@ -2,9 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
-let alertRender;
 const Alert = ({ alerts }) => 
-
     alerts !== null &&
     alerts.length > 0 && 
     alerts.map(alert => (
