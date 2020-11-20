80060df31d7c78b81d2eb4d6689bf3beb4ff2bba
Edidiong Daniel <eddyenin6@gmail.com>
2020-11-20 15:24:54 +0100

    Alert reducer

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,35 @@
-import React from 'react';
-import Fragment;
-import './App.css';
-import { Fragment } from 'react';
+import React, { Fragment } from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+import Alert from './components/layout/Alert'
+//import PropTypes from 'prop-types'
 
-const App = () => ( <
-    Fragment > < h1 > < /h1></Fragment >
-);
+//Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
+import './App.css';
+
+const App = () =>
+    <Provider store={store}>
+        <Fragment >
+            <Navbar />
+            <Router>
+                <Route exact path="/" component={Landing}/>
+                <section className="container">
+                    <Alert />
+                    <Switch>
+                        <Route exact path="/register" component={Register}/>
+                        <Route exact path="/login" component={Login} />
+                    </Switch>
+                </section>
+            </Router>
+        </Fragment>
+    </Provider>
+    
+
 
 export default App;
